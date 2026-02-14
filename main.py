a666d56c0962daf3fadd8b88f1f536e5fcbbee86
savi4m83-crypto <savi4m83@gmail.com>
2026-02-13 19:03:22 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English.
+# Find the percentage he got?
+num1=int(input("Enter your num1 :"))
+num2=int(input("Enter your num2 :"))
+num3=int(input("Enter your num3 :"))
+num4=int(input("Enter your num4 :"))
+num5=int(input("Enter your num5 :"))
+print((num1+num2+num3+num4+num5)/5)
+
